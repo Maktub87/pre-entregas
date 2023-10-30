dd3a919497f3382de6110ddb349ff9185af3f397
maktub <maktubdesarrolloweb@gmail.com>
2023-10-30 15:40:42 -0300

    preparando pre-entrega

---
+++ main.js
@@ -0,0 +1,102 @@
+
+
+/*
+
+function nombreUsuario() {
+        let bienvenido = prompt("Ingresa tu nombre")
+
+        if (bienvenido == "lautaro") {
+                alert("Bienvenido " + bienvenido + " crack😎✌")
+        } else {
+                alert("No se quien sos, espero que te guste 😉⚽")
+        }
+        alert("Veamos cuanto sabes del 10, empecemos")
+}
+
+
+function preguntasBasicas() {
+        let pregunta1 = prompt("1-¿Cual es el nombre completo de messi?")
+
+        if (pregunta1 == "andres leonel messi") {
+                alert("tu respuesta es correcta 👌")
+        } else {
+                alert("intentalo de nuevo 😖")
+        }
+
+        let pregunta2 = prompt("2-¿En que club debutó?")
+        if (pregunta2 == "barcelona") {
+                alert("Al parecer algo sabes 🤣😂")
+
+        } else {
+                alert("Segui participando rey 👻")
+        }
+
+        let pregunta3 = prompt("3-¿Quien es su idoló?")
+        if (pregunta3 == "pablo aimar") {
+                alert("Excelente ⚽🏆")
+        } else {
+                alert("Mmmm... creo que no te gusta el futbol ")
+        }
+}
+*/
+
+
+/*
+function susNumeros(){
+alert("Un poco de numeros sobre el god")
+
+let respuestaUsuario = prompt("cuantos balones de oro tiene?")
+let balonesDeOro = 7
+
+if (respuestaUsuario < 4) {
+        alert("Te quedaste corto pa")
+}
+else if (respuestaUsuario < 7 || respuestaUsuario > 4) {
+        alert("uff casi casi")
+}
+else if (respuestaUsuario === 7) {
+        alert("Claaaaro Rey, tiene 7 balones por ahora jeje")
+}
+
+}
+
+let respuestaUsuario2 = prompt("cuantos titulos ganó?")
+let titulosOficiales = 44
+
+if (respuestaUsuario2 < 40) {
+        alert("Te quedaste corto pa")
+}
+else if (respuestaUsuario2 < 44 || respuestaUsuario2 > 40) {
+        alert("uff casi casi")
+}
+else if (respuestaUsuario2 === 44) {
+        alert("Claaaaro Rey, tiene 7 balones por ahora jeje")
+}
+
+}
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+nombreUsuario()
+
+preguntasBasicas()
+
+susNumeros()*/
+
+
